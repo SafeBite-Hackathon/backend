c8c528874d6f3f3f4e6f56fab3ee4f5422da345d
Shakhzod Kudratov <shakhzodkudratov@gmail.com>
2024-11-10 02:08:38 +0500

    auth fix

--- parsing/admin.py
+++ parsing/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
+from django.db.models.query import QuerySet
+from django.http import HttpRequest
 from mptt.admin import MPTTModelAdmin, TreeRelatedFieldListFilter
 from parsing import models
+from django.db.models import Count
 
 
 @admin.register(models.FetchItem)
@@ -19,3 +22,17 @@ class RecipeAdmin(admin.ModelAdmin):
 @admin.register(models.Tag)
 class TagAdmin(MPTTModelAdmin):
     mptt_level_indent = 60
+
+    list_display = ('name', 'recipe__count')
+
+    def recipe__count(self, obj):
+        return obj.recipe__count
+
+    recipe__count.short_description = "Recipes total"
+    recipe__count.admin_order_field = "recipe__count"
+
+    def get_queryset(self, request: HttpRequest) -> QuerySet:
+        qs = super().get_queryset(request)
+        return qs.annotate(Count("recipe"))
+
+
--- safebite/settings.py
+++ safebite/settings.py
@@ -140,6 +140,9 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'DEFAULT_PAGINATION_CLASS': 'safebite.custom.CustomPaginator',
     'PAGE_SIZE': 20,
